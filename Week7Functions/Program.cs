dff52274a6993ada11fc994f60e5852ecac903ac
Marko Mets <markmets@tktk.ee>
2023-05-06 23:50:37 +0300

    add Week7Functions code

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoise = Console.ReadLine();
+
+if(userChoise == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye(); 
+}
+
+PrintHello();
+PrintGoodBye();
+
+
+
+
+static void PrintHello() // function / Method
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
+
